06bb7f49a81f22fabd3c9b597b8f461b9977b7fe
Rikkamas <fnckingunicode@yahoo.co.uk>
2015-06-11 22:19:40 +0100

    Added a temp name to the readme.md, also added a bunch more code to the editing shit.

--- projectanimu.py
+++ projectanimu.py
@@ -11,9 +11,11 @@ def main():
         usage()
     elif argv[1] == "-s":
         search()
+    elif argv[1] == "-e":
+        editepisode()
 
 def search():
-    with open('animu.txt', 'r') as inF:
+    with open('animu.txt', 'r+') as inF:
         for line in inF:
             if argv[2] in line:
                 list.append(line)
@@ -33,6 +35,39 @@ def search():
                     status = line.find("||", ep+2)
                     print(line[0:name] + "\n" + line[name+4:ep-1] + "\n" + line[ep+3:])
                     
-                
+def editepisode():
+    with open('animu.txt', 'r+') as inF:
+        for line in inF:
+            if argv[2] in line:
+                list.append(line)
+                print(list)
+                if len(list) >= 2:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    print("More than one result found for search term '" + argv[2] + "'. Please choose one.")
+                    for i in range(len(list)):
+                        end = list[i].find("||") - 1
+                        print("[" + str(i) + "] " + list[i][0:end])
+                elif len(list) == 0:
+                    print("No matches found")
+                else:
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    name = line.find("||") -1
+                    ep = line.find("||", name+2)
+                    status = line.find("||", ep+2)
+                    print(line[0:name] + "\n" + line[name+4:ep-1] + "\n" + line[ep+3:] + "\n")
+                    print("Would you like to edit this record?\n")
+                    ans = raw_input("Edit? [Y/N]: ")
+                    if ans == "y":
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print("[1]" + line[0:name] + "\n" + "[2]" + line[name+4:ep-1] + "\n" + "[3]" + line[ep+3:] + "\n")
+                        ans2 = raw_input("What would you like to edit, [1], [2] or [3]: ")
+                        if ans2 == "1":
+                            print(line[0:name])
+                        elif ans2 == "2":
+                            print(line[name+4:ep-1])
+                        elif ans2 == "3":
+                            print(line[ep+3:])
+                    else:
+                        print("Invalid answer.. Exiting..")
 
 main()
